e29de0d9225ea51486922007c21c19e4910d07c5
Diamonique Danner <diamoniquedanner@Diamoniques-MacBook-Air.local>
2022-08-06 12:04:52 -0400

    added decrease buttons but still have to add reducer functions for each

--- src/app/screens/ColorBox.tsx
+++ src/app/screens/ColorBox.tsx
@@ -14,9 +14,18 @@ const ColorBox = () => {
         <>
         <h1>This is the color box screen</h1>
         <div style={{height: 100, width: 100, backgroundColor: `rgb(${store.red}, ${store.green}, ${store.blue})`}}></div>
+        <div>
+        <h1>Increase</h1>
         <button onClick={() => dispatch(increaseRed())}>Increase Red</button>
         <button onClick={() => dispatch(increaseGreen())}>Increase Green</button>
         <button onClick={() => dispatch(increaseBlue())}>Increase Blue</button>
+        </div>
+        <div>
+            <h1>Decrease</h1>
+            <button>Decrease Red</button>
+            <button>Decrease Green</button>
+            <button>Decrease Blue</button>
+        </div>
         </>
     )
 };
