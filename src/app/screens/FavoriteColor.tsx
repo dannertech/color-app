ba96913c85f9ae71416f4c2ba01d58758c615552
Diamonique Danner <diamoniquedanner@Diamoniques-MacBook-Air.local>
2022-08-07 12:07:18 -0400

    styling updates

--- src/app/screens/FavoriteColor.tsx
+++ src/app/screens/FavoriteColor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import { RootState } from '../store';
+import FavoriteColorCard from '../components/FavoriteColorCard';
 
 const FavoriteColor = () => {
     const state = useSelector((state: RootState) => state.favoritecolor)
@@ -8,7 +9,17 @@ const FavoriteColor = () => {
     return (
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <h1>Favorite Color Screen</h1>
-        <input type="text" style={{width: '50%'}}></input>
+        <label>Name</label>
+        <input type="text" style={{width: '50%', border: 3, borderStyle: 'solid'}} title="Name"></input>
+        <label style={{marginTop: 50}}>Favorite Color</label>
+        <input type="text" style={{width: '50%',border: 3, borderStyle: 'solid'}}></input>
+        <input type="submit" value="submit" style={{marginTop: 30}}></input>
+        <div>
+            <h1>Favorite Colors!</h1>
+            {state.data.map((item) => {
+                return <FavoriteColorCard name={item.name} id={item.id} color={item.color}/>
+            })}
+        </div>
         </div>
     )
 };
