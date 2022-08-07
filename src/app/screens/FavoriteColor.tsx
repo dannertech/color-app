b3131051bdea2f40c1bfd0133a1668eef75873b7
Diamonique Danner <diamoniquedanner@Diamoniques-MacBook-Air.local>
2022-08-07 09:35:18 -0400

    added icon to icons directory

--- src/app/screens/FavoriteColor.tsx
+++ src/app/screens/FavoriteColor.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import { RootState } from '../store';
 
 const FavoriteColor = () => {
+    const state = useSelector((state: RootState) => state.favoritecolor)
+    console.log(state);
     return (
         <h1>Favorite Color Screen</h1>
+
     )
 };
 
